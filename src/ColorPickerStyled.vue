2b177d32f60d35fa5440e548845eb3de0cb21fe4
kleinfreund <philipprudloff@fastmail.com>
2022-09-10 09:25:12 +0200

    Removes unnecessary viewBox

--- src/ColorPickerStyled.vue
+++ src/ColorPickerStyled.vue
@@ -24,7 +24,6 @@
           xmlns="http://www.w3.org/2000/svg"
           width="15"
           height="15"
-          viewBox="0 0 15 15"
         >
           <path
             d="M5 0v2H1v13h12v-3h-1v2H2V5h10v3h1V2H9V0zm1 1h2v2h3v1H3V3h3z"
@@ -46,7 +45,6 @@
           xmlns="http://www.w3.org/2000/svg"
           width="16"
           height="15"
-          viewBox="0 0 16 15"
         >
           <path d="M8 15l5-5-1-1-4 2-4-2-1 1zm4-9l1-1-5-5-5 5 1 1 4-2z" fill="currentColor" />
         </svg>
@@ -102,7 +100,6 @@
       xmlns="http://www.w3.org/2000/svg"
       width="15"
       height="15"
-      viewBox="0 0 15 15"
     &gt;
       &lt;path
         d="M5 0v2H1v13h12v-3h-1v2H2V5h10v3h1V2H9V0zm1 1h2v2h3v1H3V3h3z"
@@ -124,7 +121,6 @@
       xmlns="http://www.w3.org/2000/svg"
       width="16"
       height="15"
-      viewBox="0 0 16 15"
     &gt;
       &lt;path
         d="M8 15l5-5-1-1-4 2-4-2-1 1zm4-9l1-1-5-5-5 5 1 1 4-2z"
