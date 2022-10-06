ad4c61a7da341977c38ec7dd4c2fcb8f58ed5c4e
kleinfreund <philipprudloff@fastmail.com>
2022-10-06 20:42:02 +0200

    Update dependencies and package

--- src/App.vue
+++ src/App.vue
@@ -33,40 +33,6 @@
 		<template #alpha-range-input-label>
 			<span class="visually-hidden">Alpha</span>
 		</template>
-
-		<template #copy-button>
-			<span class="visually-hidden">Copy color</span>
-
-			<svg
-				aria-hidden="true"
-				xmlns="http://www.w3.org/2000/svg"
-				width="15"
-				height="15"
-			>
-				<path
-					d="M5 0v2H1v13h12v-3h-1v2H2V5h10v3h1V2H9V0zm1 1h2v2h3v1H3V3h3z"
-					fill="currentColor"
-				/>
-
-				<path
-					d="M10 7v2h5v2h-5v2l-3-3zM3 6h5v1H3zm0 2h3v1H3zm0 2h3v1H3zm0 2h5v1H3z"
-					fill="currentColor"
-				/>
-			</svg>
-		</template>
-
-		<template #format-switch-button>
-			<span class="visually-hidden">Switch format</span>
-
-			<svg
-				aria-hidden="true"
-				xmlns="http://www.w3.org/2000/svg"
-				width="16"
-				height="15"
-			>
-				<path d="M8 15l5-5-1-1-4 2-4-2-1 1zm4-9l1-1-5-5-5 5 1 1 4-2z" fill="currentColor" />
-			</svg>
-		</template>
 	</ColorPicker>
 
 	<h2>Installation</h2>
@@ -112,23 +78,6 @@
 
 	&lt;template #alpha-range-input-label&gt;
 		&lt;span class="visually-hidden"&gt;Alpha&lt;/span&gt;
-	&lt;/template&gt;
-
-	&lt;template #copy-button&gt;
-		&lt;span class="visually-hidden"&gt;Copy color&lt;/span&gt;
-
-		&lt;svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="15" height="15"&gt;
-			&lt;path d="M5 0v2H1v13h12v-3h-1v2H2V5h10v3h1V2H9V0zm1 1h2v2h3v1H3V3h3z" fill="currentColor" /&gt;
-			&lt;path d="M10 7v2h5v2h-5v2l-3-3zM3 6h5v1H3zm0 2h3v1H3zm0 2h3v1H3zm0 2h5v1H3z" fill="currentColor" /&gt;
-		&lt;/svg&gt;
-	&lt;/template&gt;
-
-	&lt;template #format-switch-button&gt;
-		&lt;span class="visually-hidden"&gt;Switch format&lt;/span&gt;
-
-		&lt;svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="16" height="15"&gt;
-			&lt;path d="M8 15l5-5-1-1-4 2-4-2-1 1zm4-9l1-1-5-5-5 5 1 1 4-2z" fill="currentColor" /&gt;
-		&lt;/svg&gt;
 	&lt;/template&gt;
 &lt;/ColorPicker&gt;</code></pre>
 
